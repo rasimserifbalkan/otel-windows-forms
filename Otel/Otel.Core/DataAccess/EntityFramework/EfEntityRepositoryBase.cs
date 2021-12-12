41463cb2821f9ac2dbc861f04bdfa08df27814fb
rasim <rasim.serif.balkan@gmail.com>
2021-12-12 14:19:39 +0300

    start

---
+++ Otel/Otel.Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Otel.Core.DataAccess.EntityFramework
+{
+    //TContext bağlanılacak database
+    public class EfEntityRepositoryBase<Tablo,TContext> : IEntityRepository<Tablo>
+    {
+    }
+}
---
+++ Otel/Otel.Core/DataAccess/EntityFramework/IEfEntityRepositoryBase.cs
@@ -0,0 +1,18 @@
+﻿using Otel.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Linq.Expressions;
+namespace Otel.Core.DataAccess.EntityFramework
+{
+    public interface IEfEntityRepositoryBase<Tablo> where Tablo: class, IEntity , new()
+    {
+        Tablo Get(Expression<Func<Tablo, bool>> filter = null);
+        List<Tablo> List(Expression<Func<Tablo, bool>> filter = null);
+        IQueryable<Tablo> Query(Expression<Func<Tablo, bool>> filter = null);
+        void Add(Tablo entity);
+        void Update(Tablo entity);
+        void Delete(Tablo entity);
+    }
+}
